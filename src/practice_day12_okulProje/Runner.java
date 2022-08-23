cc15aa81783723386995d471164632407e83e7b1
Ersin Akun <ersinakun34@gmail.com>
2022-08-23 12:32:28 +0300

    V1

---
+++ src/practice_day12_okulProje/Runner.java
@@ -0,0 +1,13 @@
+package practice_day12_okulProje;
+
+
+import static practice_day12_okulProje.Islemler.girisPaneli;
+
+public class Runner {
+    public static void main(String[] args) {
+        girisPaneli();//static method import edildi
+        // Islemler.girisPaneli();//YANINA BOYLEDE YAZILABILIR YAZIN
+
+
+    }
+}
